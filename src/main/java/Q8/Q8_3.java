615c1f223b4cb974a260dc2983bb3d3504c47598
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-12-12 14:36:28 +0900

    確認問題8.3 コマンドラインで受け取った2つの値の合計を求めて画面に表示するプログラムQ8_3.javaを作成しました

---
+++ src/main/java/Q8/Q8_3.java
@@ -0,0 +1,11 @@
+public class Q8_3 {
+    public static void main(String[] args) {
+        int total;
+        int a1, a2;
+
+        a1 = Integer.parseInt(args[0]);
+        a2 = Integer.parseInt(args[1]);
+        total = a1 + a2;
+        System.out.println(a1 + "+" + a2 + "=" + total);
+    }
+}
