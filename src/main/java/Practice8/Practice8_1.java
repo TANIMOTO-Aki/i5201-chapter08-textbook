c1d9d905a17347b2b5aeeb7d48577bb99de2330f
TANIMOTO-Aki <kisy23011@kibireha.ac.jp>
2023-12-13 09:26:52 +0900

    演習問題8.1 Practice8_1.javaとArithmetic.javaを作成しました。 ただし、roundメソッド使用した際、実行例のように小数点が消えません。

---
+++ src/main/java/Practice8/Practice8_1.java
@@ -0,0 +1,13 @@
+public class Practice8_1 {
+    public static void main(String[] args) {
+        Arithmetic ar = new Arithmetic();       //インスタンス生成
+
+        /* テスト結果の表示 */
+        System.out.println("absolute(10) -> " + ar.absolute(10));
+        System.out.println("absolute(-10.0) -> " + ar.absolute(-10.0));
+        System.out.println("round(1.49) -> " + ar.round(1.49));
+        System.out.println("round(1.50) -> " + ar.round(1.50));
+        System.out.println("round(-1.49) -> " + ar.round(-1.49));
+        System.out.println("round(-1.50) -> " + ar.round(-1.50));
+    }
+}
